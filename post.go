249032d8ad6d9b5a504c9d899f08775083e1a3f4
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-29 17:31:35 -0300

    getting posts | v.1

--- post.go
+++ post.go
@@ -3,8 +3,17 @@ package post
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func GetPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, `{"ID": "1", "Title": "Post 1", "Content": "Post Content"}`)
+	postID := strings.TrimPrefix(r.URL.Path, "/posts/")
+	if postID == "1" {
+		fmt.Fprint(w, `{"ID": "1", "Title": "Post 1", "Content": "Post Content"}`)
+		return
+	}
+	if postID == "2" {
+		fmt.Fprint(w, `{"ID": "2", "Title": "Post 2", "Content": "Post Content"}`)
+		return
+	}
 }
--- post_test.go
+++ post_test.go
@@ -8,15 +8,30 @@ import (
 
 func TestGetPost(t *testing.T) {
 
-	request, _ := http.NewRequest(http.MethodGet, "/post/1", nil)
-	response := httptest.NewRecorder()
+	t.Run("returns post with id equal to 1", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/posts/1", nil)
+		response := httptest.NewRecorder()
 
-	GetPost(response, request)
+		GetPost(response, request)
 
-	got := response.Body.String()
-	want := `{"ID": "1", "Title": "Post 1", "Content": "Post Content"}`
+		got := response.Body.String()
+		want := `{"ID": "1", "Title": "Post 1", "Content": "Post Content"}`
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+	t.Run("returns post with id equal to 2", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/posts/2", nil)
+		response := httptest.NewRecorder()
+
+		GetPost(response, request)
+
+		got := response.Body.String()
+		want := `{"ID": "2", "Title": "Post 2", "Content": "Post Content"}`
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
